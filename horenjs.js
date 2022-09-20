b008abca6214615a0bf40f43b35d875ec0db27cc
akrouthz <akrut1919@gmail.com>
2022-09-20 22:43:25 +0100

    horen

---
+++ horenjs.js
@@ -0,0 +1,63 @@
+function corr(){
+if (document.getElementById("mot1").value === "Berlin"){
+    document.getElementById("r1").innerHTML = "Richtig"
+}
+else {document.getElementById("f1").innerHTML = "Falsch / Die richtige Lösung ist : Berlin"}
+
+if (document.getElementById("mot2").value === "Hamburg"){
+    document.getElementById("r2").innerHTML = "Richtig" }
+
+else {document.getElementById("f2").innerHTML = "Falsch / Die richtige Lösung ist : Hamburg"}
+if (document.getElementById("mot3").value === "München"){
+    document.getElementById("r3").innerHTML = "Richtig" }
+
+else {document.getElementById("f3").innerHTML = "Falsch / Die richtige Lösung ist : München"}
+
+if (document.getElementById("mot4").value === "Potsdam"){
+    document.getElementById("r4").innerHTML = "Richtig" }
+
+else {document.getElementById("f4").innerHTML = "Falsch / Die richtige Lösung ist : Potsdam "}
+
+}
+function corr2(){
+    if (document.getElementById("u2mot1").value === "01724374333"){
+        document.getElementById("u2r1").innerHTML = "Richtig"
+    }
+    else {document.getElementById("u2f1").innerHTML = "Falsch / Die richtige Lösung ist : 1724374333"}
+    
+    if (document.getElementById("u2mot2").value === "4569872"){
+        document.getElementById("u2r2").innerHTML = "Richtig" }
+    
+    else {document.getElementById("u2f2").innerHTML = "Falsch / Die richtige Lösung ist : 4569872"}
+    if (document.getElementById("u2mot3").value === "01791261869"){
+        document.getElementById("u2r3").innerHTML = "Richtig" }
+    
+    else {document.getElementById("u2f3").innerHTML = "Falsch / Die richtige Lösung ist : 1791261869"}
+    
+    if (document.getElementById("u2mot4").value === "01762211334"){
+        document.getElementById("u2r4").innerHTML = "Richtig" }
+    
+    else {document.getElementById("u2f4").innerHTML = "Falsch / Die richtige Lösung ist : 1762211334 "}
+    
+    }
+    function corr3(){
+        if (document.getElementById("u3mot1").value === "buchstabieren"){
+            document.getElementById("u3r1").innerHTML = "Richtig"
+        }
+        else {document.getElementById("u3f1").innerHTML = "Falsch / Die richtige Lösung ist : buchstabieren"}
+        
+        if (document.getElementById("u3mot2").value === "Entschuldigung"){
+            document.getElementById("u3r2").innerHTML = "Richtig" }
+        
+        else {document.getElementById("u3f2").innerHTML = "Falsch / Die richtige Lösung ist : Entschuldigung"}
+        if (document.getElementById("u3mot3").value === "wiederholen"){
+            document.getElementById("u3r3").innerHTML = "Richtig" }
+        
+        else {document.getElementById("u3f3").innerHTML = "Falsch / Die richtige Lösung ist : wiederholen"}
+        
+        if (document.getElementById("u3mot4").value === "schreiben"){
+            document.getElementById("u3r4").innerHTML = "Richtig" }
+        
+        else {document.getElementById("u3f4").innerHTML = "Falsch / Die richtige Lösung ist : schreiben "}
+        
+        }
